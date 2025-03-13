d858ee63363924f1bd2defece2b23bde2a8fd562
lolu1032 <soowkw@naver.com>
2025-03-13 18:42:03 +0900

    리펙토링(5) - display() 로직 이관

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -3,7 +3,9 @@ package cleancode.studycafe.tobe;
 import cleancode.studycafe.tobe.exception.AppException;
 import cleancode.studycafe.tobe.io.StudyCafeFileHandler;
 import cleancode.studycafe.tobe.io.StudyCafeIOHandler;
-import cleancode.studycafe.tobe.model.*;
+import cleancode.studycafe.tobe.model.pass.*;
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPasses;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,7 +21,7 @@ public class StudyCafePassMachine {
       ioHandler.showWelcomeMessage();
       ioHandler.showAnnouncement();
 
-      StudyCafePass selectedPass = selectPass();
+      StudyCafeSeatPass selectedPass = selectPass();
 
       Optional<StudyCafeLockerPass> optionalLockerPass = selectLockerPass(selectedPass);
 
@@ -34,20 +36,20 @@ public class StudyCafePassMachine {
     }
   }
 
-  private StudyCafePass selectPass() {
+  private StudyCafeSeatPass selectPass() {
     StudyCafePassType studyCafePassType = ioHandler.askPassTypeSelecting();
 
-    List<StudyCafePass> passCandiates = findPassCandidatesBy(studyCafePassType);
+    List<StudyCafeSeatPass> passCandiates = findPassCandidatesBy(studyCafePassType);
 
     return ioHandler.askPassSelecting(passCandiates);
   }
 
-  private List<StudyCafePass> findPassCandidatesBy(StudyCafePassType studyCafePassType) {
-    StudyCafePasses allPasses = studyCafeFileHandler.readStudyCafePasses();
+  private List<StudyCafeSeatPass> findPassCandidatesBy(StudyCafePassType studyCafePassType) {
+    StudyCafeSeatPasses allPasses = studyCafeFileHandler.readStudyCafePasses();
     return allPasses.findPassBy(studyCafePassType);
   }
 
-  private Optional<StudyCafeLockerPass> selectLockerPass(StudyCafePass selectedPass) {
+  private Optional<StudyCafeLockerPass> selectLockerPass(StudyCafeSeatPass selectedPass) {
     // 고정 좌석 타입이 아닌가?
     // 사물함 옵션을 사용할 수 있는 타입이 아닌가?(더 높은 추상화레벨)
     if(selectedPass.cannotUseLocker()) {
@@ -67,7 +69,7 @@ public class StudyCafePassMachine {
     return Optional.empty();
   }
 
-  private Optional<StudyCafeLockerPass> findLockerPassCandidateBy(StudyCafePass pass) {
+  private Optional<StudyCafeLockerPass> findLockerPassCandidateBy(StudyCafeSeatPass pass) {
     StudyCafeLockerPasses allLockerPasses = studyCafeFileHandler.readLockerPasses();
 
     return allLockerPasses.findLockerPassBy(pass);
--- src/main/java/cleancode/studycafe/tobe/io/StudyCafeIOHandler.java
+++ src/main/java/cleancode/studycafe/tobe/io/StudyCafeIOHandler.java
@@ -1,8 +1,8 @@
 package cleancode.studycafe.tobe.io;
 
-import cleancode.studycafe.tobe.model.StudyCafeLockerPass;
-import cleancode.studycafe.tobe.model.StudyCafePass;
-import cleancode.studycafe.tobe.model.StudyCafePassType;
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPass;
+import cleancode.studycafe.tobe.model.pass.StudyCafePassType;
 
 import java.util.List;
 
@@ -18,11 +18,11 @@ public class StudyCafeIOHandler {
     outputHandler.showAnnouncement();
   }
 
-  public void showPassOrderSummary(StudyCafePass selectedPass, StudyCafeLockerPass lockerPass) {
+  public void showPassOrderSummary(StudyCafeSeatPass selectedPass, StudyCafeLockerPass lockerPass) {
     outputHandler.showPassOrderSummary(selectedPass, lockerPass);
   }
 
-  public void showPassOrderSummary(StudyCafePass selectedPass) {
+  public void showPassOrderSummary(StudyCafeSeatPass selectedPass) {
     outputHandler.showPassOrderSummary(selectedPass);
   }
 
@@ -36,7 +36,7 @@ public class StudyCafeIOHandler {
     return inputHandler.getPassTypeSelectingUserAction();
   }
 
-  public StudyCafePass askPassSelecting(List<StudyCafePass> passCandiates) {
+  public StudyCafeSeatPass askPassSelecting(List<StudyCafeSeatPass> passCandiates) {
     outputHandler.showPassListForSelection(passCandiates);
     return inputHandler.getSelectPass(passCandiates);
   }
